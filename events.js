3d122ca6379435fedf07246417bc4a35c018e112
sethukoodaranhi <sethukoodaranhi@gmail.com>
2021-10-22 20:30:01 +0530

    initial commit

---
+++ events.js
@@ -0,0 +1,68 @@
+// onclick()
+// var item=document.getElementById("imgitem");
+// function hide(){
+//     item.setAttribute("hidden","true");
+// }
+// function reset(){
+//     item.removeAttribute("hidden","true");
+// }
+// var divEle=document.getElementById("customtext")
+// function change(){
+//     divEle.innerHTML="<p> your item is missing</p>"
+// }
+
+// mouseover()
+
+// function changeimg(){
+//     item.removeAttribute("src","./images/img1.jpg");
+//     item.setAttribute("src","./images/img2.jpg");
+// }
+
+// mouseout()
+// function resetimg(){
+//     item.removeAttribute("src","./images/img2.jpg");
+//     item.setAttribute("src","./images/img1.jpg");
+// }
+
+// onkeydown()
+//  function presskey(){
+//      alert("you pressed a key");
+//  }
+
+// onkeyup()
+
+// function txtupper(){
+//     var input=document.getElementById("txt").value;
+//     var convert;
+//     convert=input.toUpperCase();
+//     document.getElementById("txt").value=convert;
+   
+// }
+// function msg(){
+//     alert("thanks for your response")
+    
+    
+// }
+
+// onscroll
+// function scrolli(){
+//     var text=document.getElementById("para");
+//     text.innerHTML="you scrolled the div content"
+// }
+
+// ondrag()
+
+// function drag(){
+//     alert("you drag me")
+// }
+
+// ondrop()
+// function dragme(){
+//     var msg=document.getElementById("msg1")
+//     msg.innerHTML="you drag one item";
+// }
+
+// function dropme(){
+//     var message=document.getElementById("msg")
+//     message.innerHTML="you drop a new item";
+// }
